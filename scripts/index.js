cb28712c4acb7d9d788f8931e965f70a0e246923
ggoodkey <ggoodkey>
2019-03-04 10:18:07 +0200

    removed arrow function

--- scripts/index.js
+++ scripts/index.js
@@ -2356,10 +2356,10 @@
 		data: state,
 		mounted: function() {
 			this.$router.afterEach(this.updateCurrentView);
-			this.$router.beforeEach((to, from, next) => {
+			this.$router.beforeEach(function (to, from, next) {
 				// This goes through the matched routes from last to first, finding the closest route with a title.
 				// eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
-				const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+				const nearestWithTitle = to.matched.slice().reverse().find(function (r) { r.meta && r.meta.title; });
 
 				// If a route with a title was found, set the document (page) title to that value.
 				if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
