12e942ffb3f4adbd75ba306545bafb10377d5ae7
ggoodkey <ggoodkey>
2019-11-19 10:54:00 +0100

    test

--- scripts/storage.js
+++ scripts/storage.js
@@ -85,6 +85,7 @@ var APP = APP || {}, Base64, Windows, Lawnchair, dropbox, cordova, window = wind
 				}
 				// or from Lawnchair object and cache it to to LOCAL object for quick retrieval later
 				else {
+					console.log(Lawnchair);
 					LOCAL[refName] = new Lawnchair();
 					return LOCAL[refName].get(refName, got);
 				}
