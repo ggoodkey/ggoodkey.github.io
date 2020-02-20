8c500bf34c322aa737cae8c064004ea3f1017068
ggoodkey <ggoodkey>
2020-02-20 20:04:51 +0100

    load one bundled file in webworker

--- scripts/webworker.js
+++ scripts/webworker.js
@@ -12,7 +12,7 @@ require({
 	baseUrl: ".",
 	waitSeconds: 20
 },
-	["./base64", "./validate", "./nyckelDB"],
+	["./bundle"],
 	function (Base64, VAL, NyckelDB) {
 		function ab2str(buffer) {
 			var bufView = new Uint16Array(buffer),
