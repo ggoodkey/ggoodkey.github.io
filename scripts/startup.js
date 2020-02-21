a13c4b6e0aed364675f1b7eb529a9f45d3c5910c
ggoodkey <ggoodkey>
2020-02-21 20:29:56 +0100

    update docs minor dropshadow ui bugfix remove unnessisary files

--- scripts/startup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import * as Application from './main';
-// declare var require: (modules: string[], ready: Function, errback: Function) => void;
-// // Try and load platform-specific code from the /merges folder.
-// // More info at http://taco.visualstudio.com/en-us/docs/configure-app/#Content.
-// require(["./platformOverrides"],
-//     () => Application.initialize(),
-//     () => Application.initialize());
-//# sourceMappingURL=startup.js.map
